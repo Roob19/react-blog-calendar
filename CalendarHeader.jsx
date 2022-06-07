c35a6835d67ab2d562218cbbd9f9919072f04c5b
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-06 19:57:26 -0700

    making moment-utils.js and refactored CalendarHeader.jsx

--- CalendarHeader.jsx
+++ CalendarHeader.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import moment from 'moment';
+import {
+    getReadableMonthDate, 
+    getReadableWeekday, 
+    getYear,
+} from './moment-utils';
 
 const CalendarHeader = ({selectDate}) => {
     return (
         <div className="bae-calendar-header">
             <div className="left-container">
-                <h1>{moment(selectDate).format('dddd')}</h1>
-                <h1>{moment(selectDate).format('MMMM Do')}</h1>
+                <h1>{getReadableWeekday(selectDate)}</h1>
+                <h1>{getReadableMonthDate(selectDate)}</h1>
             </div>
             <div className="right-container">
-                <h3>{moment(selectDate).year()}</h3>
+                <h3>{getYear(selectDate)}</h3>
             </div>
         </div>
     );
