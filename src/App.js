ca074e9f73c59b85588ee7304f2a420b31147ef2
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-06 19:42:23 -0700

    making CalendarHeader component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, {useState} from 'react';
+import moment from 'moment';
+import './bae-calendar.scss';
+import CalendaerHeader from './calendar-header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getDaysInMonth = (month, year) => {
+  return moment(`${month}-${year}`, 'MM-YYYY').daysInMonth();
 }
 
-export default App;
+const getFirstWeekdayOfMonth = (month, year) => {
+  return moment(
+    `${month}-${year}`, 
+    'MM-YYYY'
+  ).startOf('month').weekday();
+}
+
+const getDatesInMonthDisplay = (month, year) => {
+  const daysInMonth = getDaysInMonth(month, year);
+  const firstWeekday = getFirstWeekdayOfMonth(month, year);
+  const result = [];
+
+  const prev = getPrevMonthYear(month, year);
+  const prevDaysInMonth = getDaysInMonth(
+    prev.month, 
+    prev.year
+  );
+
+  for (let j = firstWeekday - 1; j >= 0; j--) {
+    result.push({
+      date: moment(
+        `${prev.month}-${prevDaysInMonth - j}-${prev.year}`, 
+        'MM-DD-YYYY'
+      ).toDate(), 
+      currentMonth: false
+      })
+  }
+
+  for (let i = 1; i <= daysInMonth; i++) {
+    result.push({
+      date: moment(`${month}-${i}-${year}`, 'MM-DD-YYYY').toDate(), 
+      currentMonth: true
+    })
+  }
+
+  if (result.length < 42) {
+    const daysToAdd = 42 - result.length;
+    const next = getNextMonthYear(month, year);
+
+    for (let k = 1; k <= daysToAdd; k++) {
+      result.push({
+        date: moment(
+          `${next.month}-${k}-${next.year}`, 
+          'MM-DD-YYYY'
+        ).toDate(), 
+        currentMonth: false
+      })
+    }
+  }
+  return result;
+}
+
+const getPrevMonthYear = (month, year) => {
+  if (month === 1) {
+    return {
+      month: 12, 
+      year: year - 1
+    }
+  }
+  return {
+    month: month - 1, 
+    year
+  }
+}
+
+const getNextMonthYear = (month, year) => {
+  if (month === 1) {
+    return {
+      month: month + 1, 
+      year
+    }
+  }
+  return {
+    month: 12, 
+    year: year + 1
+  }
+}
+
+const BaeCalendar = () => {
+  const [selectDate, setSelectDate] = useState(moment().toDate());
+
+  return (
+    <div className={`bae-calendar-container ${themes[theme]}`}>
+      <CalendarHeader selectDate={selectDate}/>
+    </div>
+  );
+};
+
+export default BaeCalendar;
